629ac7a31a5d7f7482733a8bc41099f576d82236
Merve Gürcan <mervegrcn1@gmail.com>
2023-03-14 16:55:03 +0300

    RatingProducts_SortingReviews
    The project includes weighting product scores with amazon data and ranking them according to comments.

---
+++ casestudy_ratingproduct_and_sorting_reviews_amazon.py
@@ -0,0 +1,208 @@
+
+###################################################
+# PROJE: Rating Product & Sorting Reviews in Amazon
+###################################################
+
+###################################################
+# İş Problemi
+###################################################
+
+# E-ticaretteki en önemli problemlerden bir tanesi ürünlere satış sonrası verilen puanların doğru şekilde hesaplanmasıdır.
+# Bu problemin çözümü e-ticaret sitesi için daha fazla müşteri memnuniyeti sağlamak, satıcılar için ürünün öne çıkması ve satın
+# alanlar için sorunsuz bir alışveriş deneyimi demektir. Bir diğer problem ise ürünlere verilen yorumların doğru bir şekilde sıralanması
+# olarak karşımıza çıkmaktadır. Yanıltıcı yorumların öne çıkması ürünün satışını doğrudan etkileyeceğinden dolayı hem maddi kayıp
+# hem de müşteri kaybına neden olacaktır. Bu 2 temel problemin çözümünde e-ticaret sitesi ve satıcılar satışlarını arttırırken müşteriler
+# ise satın alma yolculuğunu sorunsuz olarak tamamlayacaktır.
+
+###################################################
+# Veri Seti Hikayesi
+###################################################
+
+# Amazon ürün verilerini içeren bu veri seti ürün kategorileri ile çeşitli metadataları içermektedir.
+# Elektronik kategorisindeki en fazla yorum alan ürünün kullanıcı puanları ve yorumları vardır.
+
+# Değişkenler:
+# reviewerID: Kullanıcı ID’si
+# asin: Ürün ID’si
+# reviewerName: Kullanıcı Adı
+# helpful: Faydalı değerlendirme derecesi
+# reviewText: Değerlendirme
+# overall: Ürün rating’i
+# summary: Değerlendirme özeti
+# unixReviewTime: Değerlendirme zamanı
+# reviewTime: Değerlendirme zamanı Raw
+# day_diff: Değerlendirmeden itibaren geçen gün sayısı
+# helpful_yes: Değerlendirmenin faydalı bulunma sayısı
+# total_vote: Değerlendirmeye verilen oy sayısı
+
+
+
+###################################################
+# GÖREV 1: Average Rating'i Güncel Yorumlara Göre Hesaplayınız ve Var Olan Average Rating ile Kıyaslayınız.
+###################################################
+
+# Paylaşılan veri setinde kullanıcılar bir ürüne puanlar vermiş ve yorumlar yapmıştır.
+# Bu görevde amacımız verilen puanları tarihe göre ağırlıklandırarak değerlendirmek.
+# İlk ortalama puan ile elde edilecek tarihe göre ağırlıklı puanın karşılaştırılması gerekmektedir.
+
+
+###################################################
+# Adım 1: Veri Setini Okutunuz ve Ürünün Ortalama Puanını Hesaplayınız.
+###################################################
+
+import pandas as pd
+import math
+import scipy.stats as st
+from sklearn.preprocessing import MinMaxScaler
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('display.float_format', lambda x: '%.5f' % x)
+
+df = pd.read_csv("/Users/mervegurcan/PycharmProjects/pythonProject/DATASETS/amazon_review.csv")
+df.head()
+df.shape
+df["overall"].value_counts()
+
+df["overall"].mean()
+
+###################################################
+# Adım 2: Tarihe Göre Ağırlıklı Puan Ortalamasını Hesaplayınız.
+###################################################
+
+df.head()
+df.info()
+df.dtypes
+
+# timestamp değişkeni object tipli olduğu için datetime formatına getiriyoruz
+df["reviewTime"] = pd.to_datetime(df["reviewTime"])
+
+df["reviewTime"].max()
+
+# bugünün tarihini belirliyoruz
+current_date = pd.to_datetime("2014-12-09")
+
+# belirlediğimiz bugünün tarihinden kaç gün önce olduğunu görmek
+df["days"] = (current_date - df["reviewTime"]).dt.days
+
+# 30 gün ve öncesinde verilen puanların ortalaması
+df.loc[df["days"] <= 30, "overall"].mean()
+
+#Out[105]: 4.742424242424242
+
+#90 gün ve öncesi, 30 güne kadar olan puanların ortalaması
+df.loc[(df["days"] > 30) & (df["days"] <= 90), "overall"].mean()
+
+#Out[108]: 4.803149606299213
+
+#90 gün sonrası, 180 güne kadar (180 dahil) olan puanların ortalaması
+df.loc[(df["days"] > 90) & (df["days"] <= 180), "overall"].mean()
+
+#Out[109]: 4.649484536082475
+
+df.loc[(df["days"] > 180), "overall"].mean()
+
+#Out[110]: 4.573373327180434
+
+# \ işareti aşağı satırdan kod yazmaya devam etmek istendiğ için konulur
+# oyların ağırlıklarını hesaplama
+df.loc[df["days"] <= 30, "overall"].mean() * 28/100 + \
+    df.loc[(df["days"] > 30) & (df["days"] <= 90), "overall"].mean() * 26/100 + \
+    df.loc[(df["days"] > 90) & (df["days"] <= 180), "overall"].mean() * 24/100 + \
+    df.loc[(df["days"] > 180), "overall"].mean() * 22/100
+
+# fonksiyonlaştırma
+def time_based_weighted_average(dataframe, w1=28, w2=26, w3=24, w4=22):
+    return dataframe.loc[df["days"] <= 30, "overall"].mean() * w1 / 100 + \
+           dataframe.loc[(dataframe["days"] > 30) & (dataframe["days"] <= 90), "overall"].mean() * w2 / 100 + \
+           dataframe.loc[(dataframe["days"] > 90) & (dataframe["days"] <= 180), "overall"].mean() * w3 / 100 + \
+           dataframe.loc[(dataframe["days"] > 180), "overall"].mean() * w4 / 100
+
+time_based_weighted_average(df)
+
+#Out[113]: 4.6987161061560725
+
+###################################################
+# Görev 2: Ürün için Ürün Detay Sayfasında Görüntülenecek 20 Review'i Belirleyiniz.
+###################################################
+
+
+###################################################
+# Adım 1. helpful_no Değişkenini Üretiniz
+###################################################
+
+# Not:
+# total_vote bir yoruma verilen toplam up-down sayısıdır.
+# up, helpful demektir.
+# veri setinde helpful_no değişkeni yoktur, var olan değişkenler üzerinden üretilmesi gerekmektedir.
+
+df["hepful_no"] = df["helpful_yes"].value_counts()
+
+###################################################
+# Adım 2. score_pos_neg_diff, score_average_rating ve wilson_lower_bound Skorlarını Hesaplayıp Veriye Ekleyiniz
+###################################################
+
+def score_pos_neg_diff(up , down):
+    return up - down
+
+def score_average_rating(up, down):
+
+    if up + down == 0:
+        return 0
+    return up / (up + down)
+
+def wilson_lower_bound(up, down, confidence=0.95):
+    """
+    Wilson Lower Bound Score hesapla
+
+    - Bernoulli parametresi p için hesaplanacak güven aralığının alt sınırı WLB skoru olarak kabul edilir.
+    - Hesaplanacak skor ürün sıralaması için kullanılır.
+    - Not:
+    Eğer skorlar 1-5 arasıdaysa 1-3 negatif, 4-5 pozitif olarak işaretlenir ve bernoulli'ye uygun hale getirilebilir.
+    Bu beraberinde bazı problemleri de getirir. Bu sebeple bayesian average rating yapmak gerekir.
+
+    Parameters
+    ----------
+    up: int
+        up count
+    down: int
+        down count
+    confidence: float
+        confidence
+
+    Returns
+    -------
+    wilson score: float
+
+    """
+    n = up + down
+    if n == 0:
+        return 0
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    phat = 1.0 * up / n
+    return (phat + z * z / (2 * n) - z * math.sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z * z / n)
+
+df["up"] = df["helpful_yes"]
+df["down"] = df["total_vote"] - df["helpful_yes"]
+
+# score_pos_neg_diff
+df["score_pos_neg_diff"] = df.apply(lambda x: score_pos_neg_diff(x["up"],
+                                                                 x["down"]), axis=1)
+
+# score_average_rating
+df["score_average_rating"] = df.apply(lambda x: score_average_rating(x["up"],
+                                                                     x["down"]), axis=1)
+
+# wilson_lower_bound
+df["wilson_lower_bound"] = df.apply(lambda x: wilson_lower_bound(x["up"],
+                                                                 x["down"]), axis=1)
+
+df.head()
+df.sort_values("wilson_lower_bound", ascending=False).head(10)
+##################################################
+# Adım 3. 20 Yorumu Belirleyiniz ve Sonuçları Yorumlayınız.
+###################################################
+
+
